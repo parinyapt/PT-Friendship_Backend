d4c48c780520855d42d1d56053c6b07df9ba163b
parinyapt <parinyapt99@gmail.com>
2023-01-18 22:24:58 +0700

    add function check env variable

--- config/config.environment.go
+++ config/config.environment.go
@@ -2,13 +2,26 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
-func EnvironmentSetup() {
+func EnvironmentFileSetup() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("[Error]->Failed to load environment file : %s", err)
+	}
+}
+
+func EnvironmentVariableCheck() {
+	var requireEnvVariableList = []string{
+		"TZ",
+	}
+
+	for _, v := range requireEnvVariableList {
+		if os.Getenv(v) == "" {
+			log.Fatalf("[Error]->Environment Variable ' %s ' is not set", v)
+		}
 	}
 }
