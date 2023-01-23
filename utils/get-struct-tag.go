21ec36f57e53cb27cdf5d5d146762db62a4c0247
parinyapt <parinyapt99@gmail.com>
2023-01-24 00:54:02 +0700

    edit return error

--- utils/get-struct-tag.go
+++ utils/get-struct-tag.go
@@ -11,7 +11,7 @@ import (
 func GetStructTag(config modelUtils.GetStructTagParam) (string, error) {
 	field, ok := reflect.TypeOf(config.SelectStruct).FieldByName(config.FieldName)
 	if !ok {
-		return "", errors.Wrap(errors.New("[Custom]->Error=False"), "[Error]->Reflect Field Error")
+		return "", errors.New("[ GetStructTag() ]->Reflect Field Error")
 	}
 
 	return string(field.Tag.Get(config.TagName)), nil
